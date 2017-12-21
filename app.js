89bb377a6ebc953dc8c93d638bcb3ca4ed68567c
tamarushin <tamarushin@gmail.com>
2017-12-20 22:15:45 -0800

    Weds

--- app.js
+++ app.js
@@ -7,26 +7,11 @@ var imagesToRender = [];
 
 //random image selector
 function randomIndex() {
-  return Math.floor(Math.random() * (img.imagePaths.length - 1));
-}
-
-function populateAllImages() {
-  for (var i = 0; i < imagePaths.length; i++){
-    new Image(imagePath[i]);
-  }
-}
-function threeImagesToRender(){
-  for(var i = 0; i < imagePaths.length; i++){
-    if(shownNow = true){
-      else
-    }    if (shownNow = false){
-          else
-        }    if (shownNow = )
-
-    }
+  return Math.floor(Math.random() * (img.imagesPaths.length - 1));
 }
 
 //constructor function
+//(path) = argument
 
 function Image(path){
   this.name = str.slice(9, -4);
@@ -37,5 +22,56 @@ function Image(path){
   this.shownLastTime = false;
   allImages.push(this);
 }
+//this is where we made all the objects. Ran all of our paths thru function Image(path)
+function populateAllImages() {
+  for (var i = 0; i < imagesPaths.length; i++){
+    new Image(imagesPath[i]);
+  }
+}
+
+function toggleTrue(){
+  for(var i = 0; i < allImages.length; i++){
+    if(allImages[i].shownNow = true){
+      allImages[i].shownLastTime = true;
+    }
+  }
+}
+function toggleFalse(){
+  for(var i = 0; i < allImages.length; i++){
+    if(allImages[i].shownLastTime = true){
+      allImages[i].shownLastTime = false;
+      allImages[i].shownNow = false;
+}
+
+// function threeItemsToRender(){
+//    this.first.src =
+//    this.second.src =
+//    this.third.src =
+
+//    function threeItemsTo Render() {
+//   const element = ({
+//     new Image().randomIndex()}.
+//
+//   );
+//   ReactDOM.render(
+//     element,
+//     document.getElementById('first')
+//   );
+// }
+
+
+
+//help from stackoverflow
+// var someimage = document.getElementById('this_one').firstChild.getAttribute("src");
+// alert(someimage);
 
 // get html element by id
+var imageOne = document.getElementById('first');
+var imageTwo = document.getElementById('second');
+var imageThree = document.getElementById('third');
+
+
+
+imageOne.addEventListener('click', threeItemsToRender);
+imageTwo.addEventListener('click', threeItemsToRender);
+imageThree.addEventListener('click', threeItemsToRender);

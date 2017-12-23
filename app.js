7e0579d82cae97f5aa5d9b2b29c8955ab2bcd0c2
tamarushin <tamarushin@gmail.com>
2017-12-23 14:08:38 -0800

    example

--- app.js
+++ app.js
@@ -1,100 +1,122 @@
+// 'use strict';
+// var imagePaths = ['./assets/bag.jpg', './assets/banana.jpg', './assets/bathroom.jpg', './assets/boots.jpg', './assets/breakfast.jpg', './assets/bubblegum.jpg', './assets/chair.jpg', './assets/cthulhu.jpg', './assets/dog-duck.jpg', './assets/dragon.jpg', './assets/pen.jpg', './assets/pet-sweep.jpg', './assets/scissors.jpg', './assets/shark.jpg', './assets/sweep.jpg', './assets/tauntaun.jpg', './assets/unicorn.jpg', './assets/usb.gif', './assets/water-can.jpg', './assets/wine-glass.jpg'];
+
 'use strict';
-var imagesPaths = ['./assets/bag.jpg', './assets/banana.jpg', './assets/bathroom.jpg', './assets/boots.jpg', './assets/breakfast.jpg', './assets/bubblegum.jpg', './assets/chair.jpg', './assets/cthulhu.jpg', './assets/dog-duck.jpg', './assets/dragon.jpg', './assets/pen.jpg', './assets/pet-sweep.jpg', './assets/scissors.jpg', './assets/shark.jpg', './assets/sweep.jpg', './assets/tauntaun.jpg', './assets/unicorn.jpg', './assets/usb.gif', './assets/water-can.jpg', './assets/wine-glass.jpg'];
-
-//list of three choices
-var allImages = [];
-var imagesToRender = [];
-
-//random image selector
-function randomIndex() {
-  return Math.floor(Math.random() * (19);
+//created the photos objects constructors
+function Photo(name, filename){
+  this.name = name;
+  this.filename = filename;
+  this.clicksCounter = 0;
+  this.displayedTimes = 0;
 }
 
-//constructor function
-//(path) = argument
+var totalClicksOnImages = 0;
+//created 4 embty rrays to hold the set of photos
+var photosOnSecondToLastScreen = [];
+var photosOnPreviousScreen = [];
+var photosOnScreen = [];
+var photos = [];
 
-function Image(path){
-  this.name = str.slice(9, -4);
-  this.path = path;
-  this.numClicks = 0;
-  this.numShown = 0;
-  this.shownNow = false;
-  this.shownLastTime = false;
-  allImages.push(this);
+//created the opjects and push them into the photos array
+photos.push( new Photo('travel bag', '/assets/bag.jpg'));
+photos.push( new Photo('banana chopper', '/assets/banana.jpg'));
+photos.push( new Photo('bathroom party', '/assets/bathroom.jpg'));
+photos.push( new Photo('rain boots', '/assets/boots.jpg'));
+photos.push( new Photo('breakfast machine','/assets/breakfast.jpg'));
+photos.push( new Photo('meat ball bubblegum','/assets/bubblegum.jpg'));
+photos.push( new Photo('plastic chair','/assets/chair.jpg'));
+photos.push( new Photo('cthulhu','/assets/cthulhu.jpg'));
+photos.push( new Photo('dog nose','/assets/dog-duck.jpg'));
+photos.push( new Photo('dragon meat','/assets/dragon.jpg'));
+photos.push( new Photo('pen caps','/assets/pen.jpg'));
+photos.push( new Photo('pet sweep','/assets/pet-sweep.jpg'));
+photos.push( new Photo('scissors holder','/assets/scissors.jpg'));
+photos.push( new Photo('shark sleeping bag','/assets/shark.jpg'));
+photos.push( new Photo('sweep mat','/assets/sweep.png'));
+photos.push( new Photo('tautaun blanket','/assets/tauntaun.jpg'));
+photos.push( new Photo('unicorn meat can','/assets/unicorn.jpg'));
+photos.push( new Photo('usb container','/assets/usb.gif'));
+photos.push( new Photo('water container','/assets/water-can.jpg'));
+photos.push( new Photo('wine glass ','/assets/wine-glass.jpg'));
+
+//getting a random number to choose a photo object out of the photos array
+function getRandomIndex(list){
+  return Math.floor(Math.random() * list.length);
 }
 
-//created the opjects and push them into the images array
-photos.push(new Photo('travel bag', './assets/bag.jpg'));
-photos.push(new Photo('banana chopper', './assets/banana.jpg'));
-photos.push(new Photo('bathroom party', './assets/bathroom.jpg'));
-photos.push(new Photo('rain boots', './assets/boots.jpg'));
-photos.push(new Photo('breakfast machine','./assets/breakfast.jpg'));
-photos.push(new Photo('meat ball bubblegum','./assets/bubblegum.jpg'));
-photos.push(new Photo('plastic chair','./assets/chair.jpg'));
-photos.push(new Photo('cthulhu','./assets/cthulhu.jpg'));
-photos.push(new Photo('dog nose','./assets/dog-duck.jpg'));
-photos.push(new Photo('dragon meat','./assets/dragon.jpg'));
-photos.push(new Photo('pen caps','./assets/pen.jpg'));
-photos.push(new Photo('pet sweep','./assets/pet-sweep.jpg'));
-photos.push(new Photo('scissors holder','./assets/scissors.jpg'));
-photos.push(new Photo('shark sleeping bag','./assets/shark.jpg'));
-photos.push(new Photo('sweep mat','./assets/sweep.png'));
-photos.push(new Photo('tautaun blanket','./assets/tauntaun.jpg'));
-photos.push(new Photo('unicorn meat can','./assets/unicorn.jpg'));
-photos.push(new Photo('usb container','./assets/usb.gif'));
-photos.push(new Photo('water container','./assets/water-can.jpg'));
-photos.push(new Photo('wine glass ','./assets/wine-glass.jpg'));
+//creating three elements and assigning them to variables
+var mainDiv = document.getElementById('clickable-images');
+var firstImage = document.createElement('first');
+var secondImage = document.createElement('second');
+var thirdImage = document.createElement('third');
 
-//this is where we made all the objects. Ran all of our paths thru function Image(path)
-function populateAllImages() {
-  for (var i = 0; i < imagesPaths.length; i++){
-    new Image(imagesPath[i]);
-  }
+newSetOfPhotos();
+
+//created a function to assign the three random photos out of the photos array to the elements by revoking the getThreeRandomPhotos function
+function newSetOfPhotos() {
+  getThreeRandomPhotos();
+  firstImage.src = photosOnScreen[0].filename;
+  photosOnScreen[0].displayedTimes++;
+  secondImage.src = photosOnScreen[1].filename;
+  photosOnScreen[1].displayedTimes++;
+  thirdImage.src = photosOnScreen[2].filename;
+  photosOnScreen[2].displayedTimes++;
+  mainDiv.appendChild(firstImage);
+  mainDiv.appendChild(secondImage);
+  mainDiv.appendChild(thirdImage);
 }
 
-function toggleTrue(){
-  for(var i = 0; i < allImages.length; i++){
-    if(allImages[i].shownNow = true){
-      allImages[i].shownLastTime = true;
+
+function getThreeRandomPhotos(){
+  photos = photos.concat(photosOnSecondToLastScreen);
+  photosOnSecondToLastScreen = photosOnPreviousScreen;
+  photosOnPreviousScreen = photosOnScreen;
+  photosOnScreen = [];
+
+  // created a var nextPhoto to keep track of the next Photo we take out of photos
+  // splice out an photo object (wich removes it from photos)
+  var nextPhoto = photos.splice(getRandomIndex(photos),9, 1);
+  // concat the array returned by splice onto photos onScreen
+  photosOnScreen = photosOnScreen.concat(nextPhoto);
+  // repeat two more times to get three images
+  nextPhoto = photos.splice(getRandomIndex(photos), 1);
+  photosOnScreen = photosOnScreen.concat(nextPhoto);
+  nextPhoto = photos.splice(getRandomIndex(photos), 1);
+  photosOnScreen = photosOnScreen.concat(nextPhoto);
+
+  return photosOnScreen;
+}
+
+//created an event handler function that increment the number of time the picture was clicked
+function clicked(event){
+  totalClicksOnImages++;
+  if ( totalClicksOnImages < 25) {
+
+    // assigning the targeted image to a variable
+    var currentImage = event.target;
+
+    //checking to see if the image element on the screen is one of the objects in the photos array
+    if (currentImage.src.includes(photosOnScreen[0].filename)){
+      photosOnScreen[0].clicksCounter++;
+      console.log(photosOnScreen[0].clicksCounter);
     }
+    if (currentImage.src.includes(photosOnScreen[1].filename)){
+      photosOnScreen[1].clicksCounter++;
+      console.log(photosOnScreen[1].clicksCounter);
+    }
+    if (currentImage.src.includes(photosOnScreen[2].filename)){
+      photosOnScreen[2].clicksCounter++;
+      console.log(photosOnScreen[2].clicksCounter);
+    }
+
+    setTimeout(newSetOfPhotos, 400);
+  }
+  else{
+    firstImage.removeEventListener('click', clicked);
+    secondImage.removeEventListener('click', clicked);
+    thirdImage.removeEventListener('click', clicked);
   }
 }
-function toggleFalse(){
-  for(var i = 0; i < allImages.length; i++){
-    if(allImages[i].shownLastTime = true){
-      allImages[i].shownLastTime = false;
-      allImages[i].shownNow = false;
-}
-
-// function threeItemsToRender(){
-//    this.first.src =
-//    this.second.src =
-//    this.third.src =
-
-//    function threeItemsTo Render() {
-//   const element = ({
-//     new Image().randomIndex()}.
-//
-//   );
-//   ReactDOM.render(
-//     element,
-//     document.getElementById('first')
-//   );
-// }
-
-
-
-//help from stackoverflow
-// var someimage = document.getElementById('this_one').firstChild.getAttribute("src");
-// alert(someimage);
-
-// get html element by id
-var imageOne = document.getElementById('first');
-var imageTwo = document.getElementById('second');
-var imageThree = document.getElementById('third');
-
-
-
-imageOne.addEventListener('click', threeItemsToRender);
-imageTwo.addEventListener('click', threeItemsToRender);
-imageThree.addEventListener('click', threeItemsToRender);
+firstImage.addEventListener('click', clicked);
+secondImage.addEventListener('click', clicked);
+thirdImage.addEventListener('click', clicked);
